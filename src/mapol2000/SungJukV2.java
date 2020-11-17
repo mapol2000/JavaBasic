82b8f94256d1780f41ed1901b8c7dad3c6db229f
mapol2000 <kimpg86@gmail.com>
2020-11-17 12:58:01 +0900

    선생님이 작성한 성적 V2

--- src/mapol2000/SungJukV2.java
+++ src/mapol2000/SungJukV2.java
@@ -27,44 +27,46 @@ public class SungJukV2 {
         int mat = 0;
         int sum = 0;
         double mean = 0.0;
-        int mean1 = (int)mean / 10;
         char grd = '가';
 
-        Scanner scanner = new Scanner(System.in);
-
-        // 입력 처리
-        System.out.println("이름을 입력하세요");
-        name = scanner.nextLine();
-        System.out.println("국어 점수를 입력하세요");
-        kor = scanner.nextInt();
-        System.out.println("영어 점수를 입력하세요");
-        eng = scanner.nextInt();
-        System.out.println("수학 점수를 입력하세요");
-        mat = scanner.nextInt();
+        // 처리
+        //성적 데이터를 키보드로 입력받기 위해
+        //Scanner 클래스 초기화
+        Scanner sc = new Scanner(System.in);
+        System.out.print("이름을 입력하세요: ");
+        name = sc.nextLine();
+        System.out.print("국어 점수를 입력하세요: ");
+        kor = sc.nextInt();
+        System.out.print("영어 점수를 입력하세요: ");
+        eng = sc.nextInt();
+        System.out.print("수학 점수를 입력하세요: ");
+        mat = sc.nextInt();
 
         sum = kor + eng + mat;
         mean = sum / 3.0;
-
-        //스위치 문
-        switch (mean1) {
-            case 9:
-                grd = '수';
-                break;
-            case 8:
-                grd = '우';
-                break;
-            case 7:
-                grd = '미';
-                break;
-            case 6:
-                grd = '양';
-                break;
-            default:
-                grd = '가';
+        //학점계산은 switch문으로 처리
+        switch ((int)(mean/10)) {
+            case 10:
+            case 9: grd = '수';break;
+            case 8: grd = '우';break;
+            case 7: grd = '미';break;
+            case 6: grd = '양';break;
+            default: grd = '가';
         }
 
-        //출력
-        System.out.printf("%s 학생의 총점은 %d점, 평균은 약 %.1f점이며 학점은 %c입니다 ",name, sum, mean, grd);
+        // String.format(형식지정자, 변수들)
+        mean = Double.parseDouble(String.format("%.1f", mean));
+
+        //결과 출력
+        System.out.println("이름: " + name);
+        System.out.println("국어: " + kor);
+        System.out.println("영어: " + eng);
+        System.out.println("수학: " + mat);
+        System.out.println("-------------");
+        System.out.println("총점: " + sum);
+        System.out.println("평균: " + mean);
+        //System.out.printf("평균: %.1f\n", mean);
+        System.out.println("학점: " + grd);
 
     }
 }
